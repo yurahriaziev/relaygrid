b45dd8d990508a5d942f77d2c19091a2e344723a
yurahriaziev <yura.hriaziev@gmail.com>
2026-08-09 23:00:23 -0400

    day 2

--- backend/tests/test_agents.py
+++ backend/tests/test_agents.py
@@ -1,12 +1,25 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from uuid import UUID
 
 test_client = TestClient(app)
 
 def test_agent_create():
     response = test_client.post('/agents', json={'name': 'robot2', 'agent_type': 'robot'})
 
+    body = response.json()
+
     assert response.status_code == 201
-    assert response.json().get('name') == 'robot2'
-    assert response.json().get('agent_type') == 'robot'
-    assert response.json().get('id')
+    assert body['name'] == 'robot2'
+    assert body['agent_type'] == 'robot'
+    assert body['id']
+
+    UUID(body['id'])
+
+def test_invalid_name():
+    response = test_client.post('/agents', json={'name': '   ', 'agent_type': 'robot'})
+
+    body = response.json()
+
+    assert response.status_code == 422
+    
